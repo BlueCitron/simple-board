0b96cad0ce05b1812d38f23d109e7145d0c51919
BlueCitron <sds901234@naver.com>
2019-09-12 01:27:29 +0900

    remove

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire" app>
+    <!-- <v-navigation-drawer app>
+
+    </v-navigation-drawer> -->
+
+    <v-app-bar color="success" app>
+      <!-- -->
+      <v-row>
+        <v-col cols="2" sm="6">
+        <v-select
+          color="success"
+          label="Solo field"
+          solo
+        ></v-select>
+      </v-col>
+      </v-row>
+
+      <v-spacer></v-spacer>
+
+
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
